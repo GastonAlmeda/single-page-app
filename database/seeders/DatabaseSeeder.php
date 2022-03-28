4ea58c0186303cdf8111b03b02261946d78d0738
Gastón Almeda <gaston@gcoop.coop>
2022-03-28 19:51:54 -0300

    Create Note migration, factory and seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Note;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +15,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory()->create([
+            'email' => 'g@admin.com',
+            'password' => bcrypt('1')
+        ]);
+
+        Note::factory(30)->create();
     }
 }
